2aaa71c272703f7084c046ccfbda422054e7ef4b
y_tatsumi@hotmail.com <57840245+ytatumi@users.noreply.github.com>
2026-01-11 15:45:38 +0100

    intial commit

---
+++ src/main/java/com/example/blogplatform/BlogplatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blogplatform/BlogplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
